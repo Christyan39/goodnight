84051d5d7c3894b73d95f74f9a241ce6d8651915
Christyan39 <christyan.aditya@gmail.com>
2025-09-09 20:19:56 +0700

    feat(migration): add indexes for user and following_user references in user_followings table

--- db/migrate/20250908134206_create_user_followings.rb
+++ db/migrate/20250908134206_create_user_followings.rb
@@ -4,6 +4,9 @@ class CreateUserFollowings < ActiveRecord::Migration[8.0]
       t.references :user, null: false, foreign_key: { to_table: :users }
       t.references :following_user, null: false, foreign_key: { to_table: :users }
 
+
+      add_index :user_followings, [:user_id]
+      add_index :user_followings, [:following_user_id]
       t.timestamps
     end
   end
