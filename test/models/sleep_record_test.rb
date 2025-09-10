62129648d534128ed260b1a398604f5f20971c3b
Christyan39 <christyan.aditya@gmail.com>
2025-09-10 21:37:11 +0700

    feat(tests): add validations and associations for User and SleepRecord models

--- test/models/sleep_record_test.rb
+++ test/models/sleep_record_test.rb
@@ -4,4 +4,16 @@ class SleepRecordTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "sleep_record is valid with valid attributes" do
+    user = users(:one)
+    sleep_record = SleepRecord.new(clock_in: Time.current, user: user, clock_out: Time.current + 1.hour, duration: 60)
+    assert sleep_record.valid?
+  end
+
+  test "sleep_record belongs to user" do
+    user = users(:one)
+    sleep_record = SleepRecord.new(clock_in: Time.current, user: user, clock_out: Time.current + 1.hour, duration: 60)
+    assert_equal user, sleep_record.user
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user is valid with valid attributes" do
+    user = User.new(name: "Test User")
+    assert user.valid?
+  end
+
+  test "user has many followings" do
+    user = User.create!(name: "Test User")
+    following = User.create!(name: "Following User")
+    user.followings.create!(following_user_id: following.id)
+    assert_equal 1, user.followings.count
+  end
+
+  test "user has many followers" do
+    user = User.create!(name: "Test User")
+    follower = User.create!(name: "Follower User")
+    follower.followings.create!(following_user_id: user.id)
+    assert_equal 1, user.followers.count
+  end
+
+  test "user has many sleep_records" do
+    user = User.create!(name: "Test User")
+    user.sleep_records.create!(clock_in: Time.current)
+    assert_equal 1, user.sleep_records.count
+  end
 end
